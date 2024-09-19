01fef08ff34f11c8ff2d4d8131a2ddb21215999a
gcnsnBNF1 <nel24021@byui.edu>
2024-09-19 12:20:47 -0500

    Everything is almost in Sync.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int inputNumber;
+        do
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            inputNumber = int.Parse(input);
+            
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+
+        } while (inputNumber != 0);
+
+        int listSum = 0;
+        foreach (int number in numbers)
+        {
+            listSum += number;
+        }
+
+        int listAmount = numbers.Count;
+        float listAverage = listSum / (float)listAmount;
+        int listMaxNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is: {listSum}");
+        Console.WriteLine($"The average is: {listAverage}");
+        Console.WriteLine($"The largest number is: {listMaxNumber}");
     }
 }
