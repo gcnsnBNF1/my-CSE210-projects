3c87c35734123fafa8ae43a34c459011ebfc2e63
gcnsnBNF1 <nel24021@byui.edu>
2024-09-19 11:09:09 -0500

    Completed Prep3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine("I'm thinking of a number between 1 and 100");
+
+        int guessNumber;
+        do
+        {
+            Console.Write("What do you guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guessNumber != magicNumber);
+
+        if (guessNumber == magicNumber)
+        {
+            Console.WriteLine("Hooray! You guessed it!");
+        }
     }
 }
