64e678db3d0a9cfaff568040de8d2f8bc4189c49
gcnsnBNF1 <nel24021@byui.edu>
2024-09-20 14:18:23 -0500

    finished final prep program. Everything is now perfectly in Sync!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string user = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int numberSquared = SquareNumber(userNumber);
+
+        DisplayResult(user, numberSquared);
     }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string usersName = Console.ReadLine();
+            return usersName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What's your favorite number? ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squareNumber = number * number;
+
+            return squareNumber;
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"Hello, {name}, the square of your number is {number}");
+        }
 }
