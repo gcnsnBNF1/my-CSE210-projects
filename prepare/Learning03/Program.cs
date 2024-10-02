73bb133fd7d8cf4b3ab1ac7f0e4168e01d821620
gcnsnBNF1 <nel24021@byui.edu>
2024-10-01 19:21:57 -0500

    Finished Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction n1 = new Fraction();
+        Console.WriteLine(n1.GetFraction());
+        Console.WriteLine(n1.GetDecimal());
+
+        Fraction n2 = new Fraction(5);
+        Console.WriteLine(n2.GetFraction());
+        Console.WriteLine(n2.GetDecimal());
+
+        Fraction n3 = new Fraction(5, 8);
+        Console.WriteLine(n3.GetFraction());
+        Console.WriteLine(n3.GetDecimal());
+
+        Fraction n4 = new Fraction(4, 13);
+        Console.WriteLine(n4.GetFraction());
+        Console.WriteLine(n4.GetDecimal());
+
+        Fraction n5 = new Fraction(7, 12);
+        Console.WriteLine(n5.GetFraction());
+        Console.WriteLine(n5.GetDecimal());
     }
 }
