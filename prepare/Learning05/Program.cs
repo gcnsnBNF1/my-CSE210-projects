02454e3ad2a6da5b2de4b0899904a2e2717721d8
gcnsnBNF1 <nel24021@byui.edu>
2024-10-16 18:25:11 -0500

    Finished Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Gavin Nelson", "Algebra");
+        string summary = assignment1.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Eve Nelson", "Trigonometry", "8.2", "1-12");
+        string mathHomework = $"{assignment2.GetSummary()}\n{assignment2.GetHomeworkList()}";
+        Console.WriteLine(mathHomework);
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Graydon Lea", "Chinese History", "The Shang Dynasty");
+        string writingEssay = $"{assignment3.GetSummary()}\n{assignment3.GetWritingInformation()}";
+        Console.WriteLine(writingEssay);
     }
 }
